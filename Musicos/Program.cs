d5d45f02c9ae0098b3c4b956b89822364260da11
S3dai <126514539+S3dai@users.noreply.github.com>
2023-03-22 19:28:26 +0000

    update 1

---
+++ Musicos/Program.cs
@@ -0,0 +1,24 @@
+﻿class Musico
+{
+    public string Nombre{get; set}
+
+    public Musico(string n)
+    {Nombre = n;}
+
+    public void Saluda()
+    {
+        Console.WriteLine("Hola, soy ");
+    }
+
+    public virtual void Toca()
+    {
+        Console.WriteLine($"{Nombre} tocando su instrumento");
+    }
+}
+class Program
+{
+    internal static void Main(string[] args)
+    {
+
+    }
+}
